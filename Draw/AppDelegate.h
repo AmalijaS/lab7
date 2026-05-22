c87eca3bfbaad62228d36345e0c2d37989f490b0
AmalijaS <vgkndhdpl@gmail.com>
2026-05-22 17:04:30 +0300

    Add files via upload

---
+++ Draw/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Draw
+//
+//  Created by user on 5/22/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Draw/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Draw
+//
+//  Created by user on 5/22/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
